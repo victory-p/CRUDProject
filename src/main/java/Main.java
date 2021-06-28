dac641675e8cd473c3b7e9c8f8bcb44cd926dd7c
박승리 <victopac@naver.com>
2021-06-29 04:51:46 +0900

    최종 업데이트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Hello World!!!!");
+    public static void main(String[] args){
+        Management m = new Management();
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            m.printMenu();
+            System.out.print(">>> ");
+            int menu = sc.nextInt();
+
+            if(m.selectMenu(menu) == false) break;
+        }
     }
 }
