74af77adc7701bd0e1c4dada3adaf0d45d1fc753
박승리 <victopac@naver.com>
2021-06-28 14:46:04 +0900

    Create MemberClass

---
+++ src/main/java/Member.java
@@ -0,0 +1,69 @@
+public class Member {
+    private int num; //넘버링
+    private String name; //이름
+    private String gender; //성별
+    private String major; //학부
+    private String studentNum; //학번
+    private String date; //등록일자
+
+    public Member(){
+
+    }
+
+    public Member(int num, String name, String gender, String major, String studentNum, String date){
+        this.num = num;
+        this.name = name;
+        this.gender = gender;
+        this.major = major;
+        this.studentNum = studentNum;
+        this.date = date;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
